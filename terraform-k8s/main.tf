c947737df910421ba58aab07f981eee671c327d0
jatinnathh <jatinnath1111@gmail.com>
2026-03-18 11:17:44 +0530

    terraform lab 8

---
+++ terraform-k8s/main.tf
@@ -0,0 +1,62 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+# Deployment
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx-deployment"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:latest"
+          name  = "nginx-container"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+# Service
+resource "kubernetes_service" "nginx_service" {
+  metadata {
+    name = "nginx-service"
+  }
+
+  spec {
+    selector = {
+      app = "nginx"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
+}
